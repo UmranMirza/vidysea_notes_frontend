4e792d3946ff809452ae7bd4865960bf9b802632
Mohd Umranuddin <129723878+UmranMirza@users.noreply.github.com>
2025-10-07 22:15:05 +0530

    Update api.js

--- notes-frontend/src/utils/api.js
+++ notes-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://vidysea-notes-backend.onrender.com/',
+   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 //   baseURL: 'http://127.0.0.1:8000/',
 });
 
